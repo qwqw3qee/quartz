3f1d76dbba9624978179f56c68c831d2bb90a08c
lazyjoy <thejanlee@foxmail.com>
2024-09-21 00:56:19 +0800

    修改配置

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "LazyJack的空间",
+    pageTitle: "LazyJack",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
