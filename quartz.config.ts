1963b5a365d371bbebcd2552c21523ccb7905de4
lazyjoy <thejanlee@foxmail.com>
2024-09-22 15:43:04 +0800

    修改文件日期配置

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "zh-CN",
     baseUrl: "lazyjack.12123123.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
