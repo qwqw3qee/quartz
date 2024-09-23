f300775a93509b99a805f198814ce0d6d1ec8eff
lazyjoy <thejanlee@foxmail.com>
2024-09-23 14:28:07 +0800

    测试日期

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -32,7 +32,7 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       if (fileData.dates && fileData.slug !== "index") {
         if (fileData.dates.created) {
           segments.push(
-            <CalendarPlus size={16} />
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-plus"><path d="M8 2v4"/><path d="M16 2v4"/><path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8"/><path d="M3 10h18"/><path d="M16 19h6"/><path d="M19 16v6"/></svg>
             <span>
               创建于 {formatDate(fileData.dates.created,cfg.locale)}
             </span>,
