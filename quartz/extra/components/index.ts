75bedfbdb10c997b91128be3dda4688fdc8b84ed
lazyjoy <thejanlee@foxmail.com>
2024-09-23 13:09:32 +0800

    删除无用组件

--- quartz/extra/components/index.ts
+++ quartz/extra/components/index.ts
@@ -1,9 +1,7 @@
 import OverlayExplorer from "./OverlayExplorer"
-import RandomPageButton from "./RandomPageButton"
 import ScrollToTop from "./ScrollToTop"
 
 export {
   OverlayExplorer,
-  RandomPageButton,
   ScrollToTop,
 }
