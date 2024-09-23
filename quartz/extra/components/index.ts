5a415da820d1d8e1dda64b59ffb86f8810f1808b
lazyjoy <thejanlee@foxmail.com>
2024-09-23 12:02:53 +0800

    增加回到顶部按钮

--- quartz/extra/components/index.ts
+++ quartz/extra/components/index.ts
@@ -1,5 +1,9 @@
 import OverlayExplorer from "./OverlayExplorer"
+import RandomPageButton from "./RandomPageButton"
+import ScrollToTop from "./ScrollToTop"
 
 export {
   OverlayExplorer,
+  RandomPageButton,
+  ScrollToTop,
 }
