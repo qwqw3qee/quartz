5fc631651971aa9ecc0e88001c65c51f3a06e6f5
lazyjoy <thejanlee@foxmail.com>
2024-09-25 22:38:26 +0800

    增加最近更新配置，优化时间显示

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -57,11 +57,55 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.RecentNotes({
+      title: "最近更新",
+      showTags: false,
+      limit: 4,
+      filter: (f) => {
+        if (f.filePath?.endsWith("index.md")) {
+          return false
+        }
+        return true
+      },
+      sort: (f1, f2) => {
+        if (f1.dates && f2.dates) {
+          if (Math.abs(f2.dates.modified.getDay() - f1.dates.modified.getDay())<=3) {
+            return f2.dates.created.getTime() - f1.dates.created.getTime()
+          }
+          return f2.dates.modified.getTime() - f1.dates.modified.getTime()
+        } else if (f1.dates && !f2.dates) {
+          return -1
+        }
+        return 1
+      }
+    })),
   ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.MobileOnly(Component.RecentNotes({
+      title: "最近更新",
+      showTags: false,
+      limit: 4,
+      filter: (f) => {
+        if (f.filePath?.endsWith("index.md")) {
+          return false
+        }
+        return true
+      },
+      sort: (f1, f2) => {
+        if (f1.dates && f2.dates) {
+          if (f2.dates.modified.getDay() == f1.dates.modified.getDay()) {
+            return f2.dates.created.getTime() - f1.dates.created.getTime()
+          }
+          return f2.dates.modified.getTime() - f1.dates.modified.getTime()
+        } else if (f1.dates && !f2.dates) {
+          return -1
+        }
+        return 1
+      }
+    })),
   ],
 }
 
