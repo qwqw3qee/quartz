7fa3a77a5a9e28f06acc204dd98ad865e0316e0f
lazyjoy <thejanlee@foxmail.com>
2024-09-26 15:53:48 +0800

    Revert "测试字体"
    This reverts commit 98b8617182c8f97f9580825e4ca73eec294be834.

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
-      fontOrigin: "local",
+      fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
